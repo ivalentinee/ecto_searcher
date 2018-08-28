834502ef66e0bcfc5d65bf02957140b087f75a79
V. Ionov <valentine.emperor@gmail.com>
2018-08-28 15:47:49 +0400

    Added sorter for, you know, sorting

---
+++ lib/ecto_searcher/sorter.ex
@@ -0,0 +1,24 @@
+defmodule EctoSearcher.Sorter do
+  @moduledoc nil
+
+  @allowed_order_values ["asc", "desc"]
+
+  require Ecto.Query
+  alias Ecto.Query
+
+  def sort(base_query, %{"field" => field, "order" => order}, sortable_fields) when is_list(sortable_fields) do
+    sortable_field_names = Enum.map(sortable_fields, &to_string/1)
+
+    if field in sortable_field_names and order in @allowed_order_values do
+      field_atom = String.to_existing_atom(field)
+      order_atom = String.to_existing_atom(order)
+      sorted_query(base_query, field_atom, order_atom)
+    else
+      base_query
+    end
+  end
+
+  defp sorted_query(base_query, field, order) when is_atom(field) and is_atom(order) do
+    Query.from(base_query, order_by: {^order, ^field})
+  end
+end
---
+++ test/ecto_searcher/sorter_test.exs
@@ -0,0 +1,48 @@
+defmodule EctoSearcher.SorterTest do
+  # NOTE: tests heavily rely on inspect(ecto_query). Sorry. I know it's awful.
+  #   http://alexkoppel.com/comparing-ecto-queries/ - this approach is no better I think
+
+  use ExUnit.Case
+
+  alias Ecto.Query
+  require Query
+
+  test "builds asc sort query" do
+    query =
+      EctoSearcher.Sorter.sort(
+        TestSchema,
+        %{"field" => "test_field_one", "order" => "asc"},
+        [:test_field_one]
+      )
+
+    expected_query = Query.from(t in TestSchema, order_by: [asc: t.test_field_one])
+
+    assert inspect(expected_query) == inspect(query)
+  end
+
+  test "builds desc sort query" do
+    query =
+      EctoSearcher.Sorter.sort(
+        TestSchema,
+        %{"field" => "test_field_one", "order" => "desc"},
+        [:test_field_one]
+      )
+
+    expected_query = Query.from(t in TestSchema, order_by: [desc: t.test_field_one])
+
+    assert inspect(expected_query) == inspect(query)
+  end
+
+  test "ignores unpermitted fields" do
+    query =
+      EctoSearcher.Sorter.sort(
+        TestSchema,
+        %{"field" => "test_field_one", "order" => "asc"},
+        [:test_field_two]
+      )
+
+    expected_query = TestSchema
+
+    assert inspect(expected_query) == inspect(query)
+  end
+end
