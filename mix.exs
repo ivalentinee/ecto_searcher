0fb0aa6528ccfc3e95e2a6b8bf8b11ceadfba691
V. Ionov <valentine.emperor@gmail.com>
2019-09-11 20:43:48 +0400

    Bump version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule EctoSearcher.MixProject do
   def project do
     [
       app: :ecto_searcher,
-      version: "0.1.1",
+      version: "0.2.0",
       elixir: "~> 1.6",
       elixirc_paths: elixirc_paths(Mix.env()),
       description: "Totally not an attempt to build Ransack-like search",
